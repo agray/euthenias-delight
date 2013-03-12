e3f330723477ab7c3d0b04cfed243f51c1cbfbff
Andrew Gray <andrew.paul.gray@gmail.com>
2013-03-12 11:57:43 +1100

    copyright update

--- Email/MailSender.cs
+++ Email/MailSender.cs
@@ -2,7 +2,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2008-2012, Andrew Gray
+ * Copyright (c) 2008-2013, Andrew Gray
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
